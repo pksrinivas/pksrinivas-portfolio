d110e69027032531fd936d4ad3ff6bc5b79c7b64
P Kaushik Srinivas <70052985+pksrinivas@users.noreply.github.com>
2025-08-04 11:20:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  base: mode === 'production' ? '/pksrinivas-portfolio/' : '/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+// Fix: Ensure `mode` is defined even in CI
+export default defineConfig(({ mode = 'production' }) => {
+  return {
+    base: mode === 'production' ? '/pksrinivas-portfolio/' : '/',
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-})
+    plugins: [
+      react(),
+      mode === 'development' && componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
