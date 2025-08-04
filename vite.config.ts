8f09f16fb7eada43280a0dad683ec5bbdc042b38
P Kaushik Srinivas <70052985+pksrinivas@users.noreply.github.com>
2025-08-04 10:53:48 +0530

    Update vite.config.ts
    Added build: {
        outDir: "docs", // 👈 THIS IS MANDATORY FOR GITHUB PAGES
        emptyOutDir: true,
      },

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,8 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "docs", // 👈 THIS IS MANDATORY FOR GITHUB PAGES
+    emptyOutDir: true,
+  },
 }));
