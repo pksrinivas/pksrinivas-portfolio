00fd470705e1dcf2f5bd898f1ea8eada37c7fcd7
P Kaushik Srinivas <70052985+pksrinivas@users.noreply.github.com>
2025-08-04 11:17:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  base: '/pksrinivas-portfolio/', 
+  base: mode === 'production' ? '/pksrinivas-portfolio/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
